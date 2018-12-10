59fcd6e2d3d3e20c07878112cc08c1cad8cc93c7
SpankerTroll <spankertroll77@gmail.com>
2018-12-10 13:13:48 +0100

    first commit

---
+++ src/main/java/com/bartmont/pollutionapp/PollutionappApplication.java
@@ -0,0 +1,12 @@
+package com.bartmont.pollutionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollutionappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PollutionappApplication.class, args);
+    }
+}
