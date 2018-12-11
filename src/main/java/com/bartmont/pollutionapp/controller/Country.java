420fd3332ed27b41058d0b10adc2aa8e2757177a
SpankerTroll <spankertroll77@gmail.com>
2018-12-11 13:20:54 +0100

    adding

--- src/main/java/com/bartmont/pollutionapp/controller/Country.java
+++ src/main/java/com/bartmont/pollutionapp/controller/Country.java
@@ -1,4 +1,40 @@
 package com.bartmont.pollutionapp.controller;
 
 public class Country {
+    private String name;
+    private String capitalName;
+    private int id;
+
+    public Country(String name, String capitalName, int id) {
+        this.name = name;
+        this.capitalName = capitalName;
+        this.id = id;
+    }
+
+    public Country() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapitalName() {
+        return capitalName;
+    }
+
+    public void setCapitalName(String capitalName) {
+        this.capitalName = capitalName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/bartmont/pollutionapp/controller/CountryContreoller.java
+++ src/main/java/com/bartmont/pollutionapp/controller/CountryContreoller.java
@@ -1,4 +1,23 @@
 package com.bartmont.pollutionapp.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CountryContreoller {
+    @RequestMapping("getCountrys")
+    public List<Country> getCountrys() {
+        List<Country> result = new ArrayList<>();
+
+        result.add(new Country("Poland", "Warsaw", 1));
+        result.add(new Country("Germany", "Berlin", 2));
+
+        return result;
+    }
 }
