a0c59112286e2a98e6624383a217cc9aedfc1bb1
SpankerTroll <spankertroll77@gmail.com>
2018-12-20 15:00:47 +0100

    Adding "Space"

--- src/main/java/com/bartmont/pollutionapp/controller/CountryController.java
+++ src/main/java/com/bartmont/pollutionapp/controller/CountryController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class CountryController {
+public class CountryContreoller {
     @Autowired
     private CountryService countryService;
 
